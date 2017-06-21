e981e8655128b05defd2409b73cf8950aa037225
Roberto Salas <r@huh.mx>
2017-06-21 13:31:19 -0500

    Changes were made to the system.

--- gulp/tasks/gulp_pug.js
+++ gulp/tasks/gulp_pug.js
@@ -4,13 +4,7 @@ var gulp = require('gulp'),
     config = require('./../gulp.config.json');
 
 gulp.task('pug', function () {
-  return gulp.src(config.pug.src + config.pug.mainFile)
-    .pipe(pug({
-        data: {
-            title: config.pug.data.title,
-            css_to_include: config.pug.data.css_to_include,
-            js_to_include: config.pug.data.js_to_include,
-        }
-    }))
+  return gulp.src(config.pug.src + "*.pug")
+    .pipe(pug())
     .pipe(gulp.dest(config.distribution_folder + config.pug.dest));
 });
